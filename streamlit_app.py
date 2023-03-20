65c27c97686bd665bcd331790e86ffc0f5483d09
BriceGH <128230273+BriceGH@users.noreply.github.com>
2023-03-20 16:41:05 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,10 +12,17 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 #Cours  Lesson 3: A Quick and Easy Streamlit App!  🥋 Importing pandas
 
+#Cours  Lesson 3: A Quick and Easy Streamlit App!  🥋 Importing pandas 
+
 import pandas
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 fruits_to_show = my_fruit_list.loc[fruits_selected]
+
+# Let's put a pick list here so they can pick the fruit they want to include 
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
+#afficher tableau
 streamlit.dataframe(my_fruit_list)
 
 #my_fruit_list = my_fruit_list.set_index('Fruit')
