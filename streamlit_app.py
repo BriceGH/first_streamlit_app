5be7dd18577ef0d9cbdfeedb5ad6ba115faf6d5f
BriceGH <128230273+BriceGH@users.noreply.github.com>
2023-03-24 15:41:10 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,9 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 #Cours  Lesson 3: A Quick and Easy Streamlit App!  🥋 Importing pandas 
 
+streamlit.stop()
 import pandas
+
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 #fruits_to_show = my_fruit_list.loc[fruits_selected]
