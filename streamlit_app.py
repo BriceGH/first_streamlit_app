8075e27f0c1b100f0663be6d71ede73aa96dfacf
BriceGH <128230273+BriceGH@users.noreply.github.com>
2023-03-24 20:33:38 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,6 +21,7 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+
 streamlit.text(fruityvice_response)
 
 fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
@@ -30,11 +31,20 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice fruits Advice!")
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?')         
-
+Try:
+   
+fruit_choice = streamlit.text_input('What fruit would you like information about?')  
+if not fruit_choice:
+   streamlit.error("Please select a fruit to get information.")
+else:
+   
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
+
+except URLError as e:
+
+streamlit.error()
 
 streamlit.stop()
 
