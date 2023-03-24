d42b14202ef9cdd4f96b11b33e0c7d0f1f337b3d
BriceGH <128230273+BriceGH@users.noreply.github.com>
2023-03-24 21:10:46 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,6 @@ streamlit.text('🥑🍞 Hard-Boiled Free-Range Egg')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 #Cours  Lesson 3: A Quick and Easy Streamlit App!  🥋 Importing pandas
-
 #Cours  Lesson 3: A Quick and Easy Streamlit App!  🥋 Importing pandas 
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
@@ -30,6 +29,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice fruits Advice!")
+
 try:   
 fruit_choice = streamlit.text_input('What fruit would you like information about?')  
 if not fruit_choice:
@@ -40,7 +40,6 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
 except URLError as e:
-
 streamlit.error()
 
 streamlit.stop()
