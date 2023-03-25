2ba97929c657c1f584fa5f123e3324f431a1c1d3
BriceGH <128230273+BriceGH@users.noreply.github.com>
2023-03-25 13:35:20 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import requests
 import snowflake.connector
 import URLError
 
-#from urllib.error import URLError
+from urllib.error import URLError
 
 streamlit.title('My Parents New Healthy Diner')
    
