e0f1441c9405edf5f835bb3d1c0d6dcb5410cb48
BriceGH <128230273+BriceGH@users.noreply.github.com>
2023-03-25 14:21:53 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,20 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header('Fruityvice fruits Advice!')
 
+
+
+
+
+
 fruit_choice = streamlit.text_input('What fruit would you like information about?','kiwi')  
+
+
+
+
+
+
+
+
    
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
