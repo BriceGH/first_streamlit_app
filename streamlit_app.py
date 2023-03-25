f81f1c121ee7dea289bd062acc12140e6394885f
BriceGH <128230273+BriceGH@users.noreply.github.com>
2023-03-25 13:00:57 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,7 +32,7 @@ try:
  
 fruit_choice = streamlit.text_input('What fruit would you like information about?')  
 
-if fruit_choice:
+if not fruit_choice:
   streamlit.error("Please select a fruit to get information.")
 
 else:         
